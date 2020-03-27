fcee91f3fd7f9c6df4c32aca6f766fd4913eff7f
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-27 11:34:40 -0400

    register and login working before hooking up sessions

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,48 @@
+const bcrypt = require('bcryptjs');
+
 const router = require('express').Router();
+
+const Users = require('../database/users-model');
 
 router.post('/register', (req, res) => {
   // implement registration
+  const userInfo = req.body;
+
+  const ROUNDS = process.env.ROUNDS || 8;
+  const hash = bcrypt.hashSync(userInfo.password, ROUNDS);
+
+  userInfo.password = hash;
+
+  Users.add(userInfo)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: 'could not add user to database'})
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  const {username, password} = req.body;
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      console.log('user', user)
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // req.session.user = {
+        //   id: user.id,
+        //   username: user.username
+        // }
+
+        res.status(200).json({user})
+      } else {
+        res.status(401).json({message: 'Please provide valid credentials'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: 'Error finding a user by that name'})
+    })
 });
 
 module.exports = router;
